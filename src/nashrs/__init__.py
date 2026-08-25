d02de2ab72bf097301dd87eafcf4fa9a647d3082
JiancongXiao <42569308+JiancongXiao@users.noreply.github.com>
2026-08-25 23:12:42 +0800

    Add general-preference reward mixture support

--- src/nashrs/__init__.py
+++ src/nashrs/__init__.py
@@ -12,7 +12,13 @@ from .nash_rs import NashRSConfig, NashRSRewardConstructor
 from .preference_oracles import (
     BTLComponent,
     MixtureBTLPreferenceOracle,
+    TransformersChatRewardOracle,
     TransformersScalarRewardOracle,
+    build_preference_oracle,
+)
+from .general_preference_diagnostics import (
+    aggregate_diagnostics,
+    diagnose_score_mixture,
 )
 from .runner import IterationResult, PPOBackend, PhaseResult, run_iteration
 from .types import Accounting, RewardBatch
@@ -34,6 +40,10 @@ __all__ = [
     "ScalarRewardOracle",
     "TextSampler",
     "TransformersScalarRewardOracle",
+    "TransformersChatRewardOracle",
+    "build_preference_oracle",
+    "diagnose_score_mixture",
+    "aggregate_diagnostics",
     "evaluate_pairwise",
     "run_iteration",
 ]
